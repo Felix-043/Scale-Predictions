6ae5e3350b3cf5e0e05c9dbced439c8701a5d949
Felix Onyango <onyangof289@gmail.com>
2024-02-01 07:25:51 +0300

    Country Info

---
+++ Countryinfo.py
@@ -0,0 +1,9 @@
+from countryinfo import CountryInfo
+# user input for Country Name
+count = input("Enter your country: ")
+country = CountryInfo(count)
+print("Capital is : ",country.capital())
+print("Currencies is : ",country.currencies())
+print("Language is : ",country.languages())
+print("Borders are : ",country.borders())
+print("Other names : ",country.alt_spellings())
